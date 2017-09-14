1921a91f27654160fdf66cd4528a79bab2ba91da
eacallam <eacallam@yahoo.com>
2017-09-13 20:10:38 -0400

    First Commit

---
+++ InheritanceAndPolymorphisms/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphisms
+{
+    class Vehicle
+    {
+        //FIELDS
+        protected int seats;
+        protected int carryingCapacity;
+        protected string color;
+        protected double movementSpeed;
+        protected double distanceTraveled;
+    }
+}
